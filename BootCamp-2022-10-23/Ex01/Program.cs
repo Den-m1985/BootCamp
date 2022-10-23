5d9710895ec4c087da7513b7f0b4f4de325022c6
den-m1985 <denismartyanov244@gmail.com>
2022-10-23 16:38:18 +0200

    wrote program

--- BootCamp-2022-10-23/Ex01/Program.cs
+++ BootCamp-2022-10-23/Ex01/Program.cs
@@ -1,1 +1,17 @@
-﻿// 
+﻿// Бинарный поиск.
+//y = log(n)
+
+#region y = log(n)
+int BinarnySearch(int[] array, int find)
+{
+    int left = 0;
+    int right = array.Length - 1;
+    while (left <= right)
+    {
+        int midle = (left + right)/2;
+        if (find == array[midle]) return midle;
+        else if (find < array[midle]) right = midle - 1;
+        else left = midle +1;
+    }
+    return -1;
+}
