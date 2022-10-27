ea882a2ea1e12087a10218e82c5182827f2f37d9
den-m1985 <denismartyanov244@gmail.com>
2022-10-27 12:13:35 +0200

    4

---
+++ BootCamp-2022-10-26/Ex01/Program.cs
@@ -0,0 +1,40 @@
+﻿// Быстрая сортировка.
+
+
+int [] array = {0,2,3,5,-2,10,356,-2158};
+int [] res = QuickSort(array, 0, array.Length - 1);
+Console.WriteLine("Sotr array: " + "[" + string.Join(" ", res) + "]");
+
+
+int [] QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return inputArray;
+    int pivod = GetPivedIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivod-1);
+    QuickSort(inputArray, pivod+1, maxIndex);
+
+    return inputArray;
+}
+
+int GetPivedIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivodIndex = minIndex-1;
+    for (int i = minIndex; i < maxIndex-1; i++)
+    {
+        if (inputArray[i]< inputArray[maxIndex])
+        {
+            pivodIndex++;
+            Swap(inputArray, i, pivodIndex);
+        }
+    }
+    pivodIndex++;
+    Swap(inputArray, pivodIndex, maxIndex);
+    return pivodIndex;
+}
+
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
