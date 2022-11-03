3225c5c584e1eb1198c7f85d186a2ff69e15ae08
den-m1985 <denismartyanov244@gmail.com>
2022-11-03 12:52:57 +0200

    faund_faces

---
+++ BootCamp-2022-11-02/VIDEO.py
@@ -0,0 +1,43 @@
+
+import cv2
+
+#обращаемся к библиотеке cv2 и класификатору используем "подготовленную"
+#нейронную сеть "cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+#по сути обращение к уже обученной нейронной сети
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#img = cv2.imread('face.jpg')
+
+#получаем значение с видеокамеры
+cap = cv2.VideoCapture(0)
+
+#вечный цикл вывода картинки с камеры
+while True:
+    #получаем значение с камеры в переменную frame с помощью метода read
+    success, frame = cap.read()
+    #success, frame = img.read()
+
+    #вывод изображения до обработки нейронной сетью
+    #cv2.imshow('camera', frame)
+
+    #конвертируем из цветной в серую картинку что бы сетка работала корректно 
+    #с серыми цветами нейронные сети работают более уверенно
+    cap_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #обращаемся к класификатору а затем и методу detectMultiScale
+    #для поиска координат в которых находится лицо человека
+    faund_faces = face_cascades.detectMultiScale(cap_grey)
+
+    #испотльзует цикл что бы работать с массивом
+    for (x, y, w, h) in faund_faces:
+        #"рисуем" прямоугольник обозначающий лицо 
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    #выводим "отредактированное" изображение
+    cv2.imshow('after processing', frame)
+
+    #выходим из цикла нажатием клавиши "q"
+    # 1 - время обновления кадра 
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
